2cf20d01a7a3c15d7d797534b4b22e4a9920b140
CLeeBenjamin <castonboyd@gmail.com>
2023-10-19 17:06:34 -0400

    feat: added dependencies,server and mock data file

---
+++ models/data.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        id: 1111,
+        item_name: "burger",
+        amount: 200,
+        date: '2023-10-19',
+        from: "jane",
+        category: 'restaurant'
+    },
+    {
+        id: 1122,
+        item_name: "soda",
+        amount: 50,
+        date: '2023-10-20',
+        from: "john",
+        category: 'drinks'
+    },
+    {
+        id: 1133,
+        item_name: "pizza",
+        amount: 250,
+        date: '2023-10-18',
+        from: "alice",
+        category: 'restaurant'
+    },
+    {
+        id: 1144,
+        item_name: "book",
+        amount: 100,
+        date: '2023-10-21',
+        from: "bookstore",
+        category: 'education'
+    },
+    {
+        id: 1155,
+        item_name: "shirt",
+        amount: 300,
+        date: '2023-10-17',
+        from: "mall",
+        category: 'clothing'
+    }
+]
