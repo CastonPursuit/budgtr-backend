6811cee6a85a054750e18f8444c0475a75997001
CLeeBenjamin <castonboyd@gmail.com>
2023-10-19 17:15:32 -0400

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const 
+const server = require('./app');
+const PORT = 3000;
+
+server.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
